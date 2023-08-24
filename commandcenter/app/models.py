09c13d9a16337e85c6e20ee73a962da17a9b337f
Eric Emery <ericemery099@gmail.com>
2023-08-24 13:27:44 +0000

    debug

--- commandcenter/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
-
-class CustomUser(AbstractUser):
-    
-    def __str__(self):
-        return self.username
